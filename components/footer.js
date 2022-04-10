fa6a65792fa56e99eefaa9a212e7f651440e4190
Agnes <agnes.beviz@gmail.com>
2022-04-10 20:29:19 +0100

    footer component created and added to blog page (test)

---
+++ components/footer.js
@@ -0,0 +1,65 @@
+class Footer extends HTMLElement {
+    constructor() {
+      super();
+    }
+
+    connectedCallback() {
+      this.innerHTML = `
+      <style>
+/*------Footer section------*/
+footer {
+  background-color: var(--main-colour);
+  margin-top: -0.5em;
+  padding: 0.3em;
+  position: fixed; 
+  bottom: 0;
+  width: 100vw;
+}
+
+#social-media-links {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 0.8rem;
+  padding-top: 0;
+}
+
+#copyright {
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  margin-top: 0.5rem;
+}
+
+/* social media connect icons*/
+.fa {
+  font-size: 1em;
+  width: 40px;
+  text-align: center;
+  text-decoration: none;
+  color: var(--accent-colour);
+}
+
+/* hover effect for social media connect icons */
+.fa:hover {
+  opacity: 0.6;
+}
+     </style>
+
+     <footer id="footer"> 
+      <section id="social-media-links">
+        <a href="https://twitter.com/mx_coder_" class="fa fa-twitter" target="_blank"></a>
+        <a href="https://www.linkedin.com/in/agnes-beviz/" class="fa fa-linkedin" target="_blank"></a>
+        <a href="https://www.freecodecamp.org/Agnes_Beviz" class="fa fa-free-code-camp" target="_blank"></a>
+        <a href="https://github.com/mxkoder" class="fa fa-github" target="_blank"></a>
+        <a href="https://www.agnesbeviz.co.uk/"><img alt="Cartoonised portrait" src="/images/portrait-ab-stylized.png" width=30px height=30px></a>
+      </section>
+      <p id="copyright">Copyright &#169 2022 Agnes Beviz</p>
+        
+    </footer>
+`;
+    }
+  }
+  
+customElements.define('nav-component', Footer);
